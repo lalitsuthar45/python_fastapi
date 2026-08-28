c5043eee2858af503e787447f7d35b6efd4a660f
lalitsuthar45 <sutharlalit791@gmail.com>
2026-08-28 12:37:33 +0530

    Update database configuration

--- database.py
+++ database.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:lalit%20suthar@localhost/fastapi_db"
+DATABASE_URL = "mysql+pymysql://root:Lalit%4012345@localhost:3306/fastapi_db"
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
